e0af72674c20e165735588c5bb08640b93e00689
gimminjae <min356812@naver.com>
2022-07-14 19:37:03 +0900

    AppTest에서 Io관련 테스트 제거

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -75,32 +75,5 @@ public class AppTest {
         assertTrue(1 == 1);
         assertEquals(1, 1);
     }
-    @Test
-    public void 스캐너에_키보드가_아닌_문자열을_입력으로_설정() {
-        Scanner sc = TestUtil.genScanner("안녕");
 
-        String cmd = sc.nextLine().trim();
-        assertEquals("안녕", cmd);
-    }
-    @Test
-    public void 문자열을_파일에_저장() {
-        //파일 생성
-        Util.file.mkdir("test_data");
-
-        //쓰기
-        Util.file.saveToFile("test_data/1.txt", "안녕");
-
-        //읽기
-        String body = Util.file.readFromFile("test_data/1.txt");
-
-        assertEquals("안녕", body);
-
-    }
-    @Test
-    public void 출력을_모니터에_하지_않고_문자열로_얻기() {
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
-        System.out.print("안녕");
-        String rs = output.toString();
-        TestUtil.clearSetOutToByteArray(output);
-    }
 }
